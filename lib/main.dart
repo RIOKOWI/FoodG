6220c42ff5c919e34e974e956e7115fcb52235b7
RIOKOWI <achyarrio2004@gmail.com>
2025-11-24 10:18:01 +0700

    feat: add stateless widget FoodG

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FoodG());
 }
 
+class FoodG extends StatelessWidget {
+  const FoodG({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food.G',
+    );
+  }
+}
