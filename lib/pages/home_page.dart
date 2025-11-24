595164ddddf2b49e6da8e00d83203db0564ea640
RIOKOWI <achyarrio2004@gmail.com>
2025-11-24 10:30:02 +0700

    feat: add text Food. and styling

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,32 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+            appBar: AppBar(
+              title: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Food.',
+                      style: TextStyle(
+                        color: Color.fromRGBO(26, 88, 169, 1),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25
+                      ),
+                    )
+                  ]
+                )
+              ),
+              actions: const [],
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: const Column(
+                  children: [],
+                ),
+              ),
+            ),
+          );
   }
 }
